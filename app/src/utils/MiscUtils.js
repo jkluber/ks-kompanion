b3f39c45e97163a3700b5bc0ae57cfc4f774ba4d
Jake Kluber <jakekluber@gmail.com>
2025-09-19 15:27:25 -0500

    Random Events

--- app/src/utils/MiscUtils.js
+++ app/src/utils/MiscUtils.js
@@ -1,5 +1,8 @@
 import * as Application from 'expo-application';
 import * as Device from 'expo-device';
+import { randoms } from '../../../assets/config/RandomEvents.js'
+import { View, Text, Image, StyleSheet } from 'react-native';
+import RunescapeText from '../components/RunescapeText';
 
 export const checkNewUser = async(deviceId, userName) => {
     const result = await sendRequest("POST", JSON.stringify({
@@ -22,7 +25,24 @@ export const rollRandomEvent = async(deviceId) => {
             return "You rolled a mega random!!!!"
         } else {
             let roll = Math.floor(Math.random() * 20);
-            return "You rolled this number: " + roll
+            return (
+                <View style={styles.randomEventView}>
+                    <RunescapeText
+                        font="RunescapeBold"
+                        fontSize={24}
+                        style={styles.title}>
+                        {randoms[roll].title}
+                    </RunescapeText>
+                    <RunescapeText
+                        fontSize={22}
+                        style={styles.description}>
+                        {randoms[roll].description}
+                    </RunescapeText>
+                    <Image
+                        style={styles.image}
+                        source={randoms[roll].image}/>
+                </View>
+            );
         }
     } else {
         return "Zero doin' right now, Kong-dude!"
@@ -55,3 +75,25 @@ export const sendRequest = async(method, body) => {
     const result = await response.json();
     return result;
 }
+
+const styles = StyleSheet.create({
+    randomEventView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 20
+    },
+    title: {
+        textAlign: "center",
+        marginBottom: 24
+    },
+    description: {
+        fontWeight: "bold",
+        marginBottom: 10
+    },
+    image: {
+        width: 120,
+        height: 120,
+        resizeMode: "contain"
+    }
+});
---
+++ assets/config/RandomEvents.js
@@ -0,0 +1,142 @@
+export const randoms = {
+    0: {
+        title: "Rick Turpentine",
+        description: "Atone for your sins! Give the poorest player 5m. If you are the poorest player, receive 5m.",
+        reward: null,
+        unit: null,
+        image: require("../../assets/images/RickTurpentine.png")
+    },
+    1: {
+        title: "Count Check",
+        description: "You have been gifted a small XP lamp!",
+        reward: "smallLamp",
+        unit: 1,
+        image: require("../../assets/images/CountCheck.png")
+    },
+    2: {
+        title: "Drunken Dwarf",
+        description: "Take 5 drinks! *hic*",
+        reward: null,
+        unit: null,
+        image: require("../../assets/images/DrunkDwarf.png")
+    },
+    3: {
+        title: "Drill Demon",
+        description: "Atten-SHUN! You've been given 100 combat XP!",
+        reward: "combat",
+        unit: 100,
+        image: require("../../assets/images/DrillDemon.png")
+    },
+    4: {
+        title: "Evil Bob",
+        description: "Mow! You've been given 100 thieving XP!",
+        reward: "thieving",
+        unit: 100,
+        image: require("../../assets/images/EvilBob.png")
+    },
+    5: {
+        title: "Freaky Forester",
+        description: "You've been given 100 gathering XP!",
+        reward: "gathering",
+        unit: 100,
+        image: require("../../assets/images/FreakyForester.png")
+    },
+    6: {
+        title: "Grave Digger",
+        description: "You've been given 100 survival XP!",
+        reward: "survival",
+        unit: 100,
+        image: require("../../assets/images/GraveDigger.png")
+    },
+    7: {
+        title: "Frogs",
+        description: "You've been given 100 magic XP! *ribbit*",
+        reward: "magic",
+        unit: 100,
+        image: require("../../assets/images/Frog.png")
+    },
+    8: {
+        title: "Mime Show",
+        description: "Mime anything! Both you and whoever guesses what you're miming correctly first receive 1m.",
+        reward: null,
+        unit: null,
+        image: require("../../assets/images/Mime.png")
+    },
+    9: {
+        title: "Mysterious Old Man",
+        description: "You get 5m!",
+        reward: null,
+        unit: null,
+        image: require("../../assets/images/MOM.png")
+    },
+    10: {
+        title: "Quiz Master",
+        description: "I haven't decided what to do with this one yet.",
+        reward: null,
+        unit: null,
+        image: require("../../assets/images/QuizMaster.png")
+    },
+    11: {
+        title: "Sandwich Lady",
+        description: "I haven't decided what to do with this one yet.",
+        reward: null,
+        unit: null,
+        image: require("../../assets/images/SandwichLady.png")
+    },
+    12: {
+        title: "Strange Plant",
+        description: "I haven't decided what to do with this one yet.",
+        reward: null,
+        unit: null,
+        image: require("../../assets/images/StrangePlant.png")
+    },
+    13: {
+        title: "Pillory Guard",
+        description: "Go to timeout or pay 5m!",
+        reward: null,
+        unit: null,
+        image: require("../../assets/images/PilloryGuard.png")
+    },
+    14: {
+        title: "Capt' Arnav",
+        description: "Parley wi' me! You get 1m.",
+        reward: null,
+        unit: null,
+        image: require("../../assets/images/Arnav.png")
+    },
+    15: {
+        title: "Genie",
+        description: "You have been gifted a large XP lamp!",
+        reward: "largeLamp",
+        unit: 1,
+        image: require("../../assets/images/GenieRandom.png")
+    },
+    16: {
+        title: "Evil Twin",
+        description: "You and another player of your choice must think of a number between 1 and 10. If it's the same number, you both receive 5m.",
+        reward: null,
+        unit: null,
+        image: require("../../assets/images/EvilTwin.png")
+    },
+    17: {
+        title: "Prison Pete",
+        description: "You have been gifted a small XP lamp!",
+        reward: "smallLamp",
+        unit: 1,
+        image: require("../../assets/images/PrisonPete.png")
+    },
+    18: {
+        title: "Postie Pete",
+        description: "You have been gifted a large XP lamp!",
+        reward: "largeLamp",
+        unit: 1,
+        image: require("../../assets/images/Pete.png")
+    },
+    19: {
+        title: "Purple Chest!",
+        description: "You get 20m!",
+        reward: null,
+        unit: null,
+        image: require("../../assets/images/PurpleChest.png")
+    }
+}
